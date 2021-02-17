81070737cb399e6c9b0f00e1097c07213af90c51
Will <wmalos22@gmail.com>
2021-02-16 16:54:11 -0800

    Added pseudo code

---
+++ script.js
@@ -0,0 +1,31 @@
+// create event listener for start button
+// starts timer and first question
+
+// create function that runs timer
+// stop timer at zero
+// call game over function at zero
+// include set interval in timer function
+
+// create array for questions, answers, wrong answers
+// make it an array of objects
+var questions = [
+    {
+        q: "String", 
+        choices: ["choice a", "choice b", "choice c"],
+        correct: "choice a" 
+    },
+    {
+
+    }
+]
+
+// create function that delivers question and choices
+// function that checks answers then goes on to next question
+// subtract time on clock with incorrect answers
+// onClick event listener
+// look up add click on button (google)
+
+// create game over function
+// stops timer
+
+// create scoreboard function into local storage
